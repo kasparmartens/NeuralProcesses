cac7ac57e066425b005629d496942bb15e205242
kasparmartens <kaspar.martens@gmail.com>
2018-08-06 15:26:41 +0100

    fitting a GP using gpflow

--- GP_helpers.R
+++ GP_helpers.R
@@ -10,3 +10,29 @@ rbf_kernel <- function(X, Y, ls, var){
   var * exp(-0.5 * square)
 }
 
+# helper function for fitting GP using gpflow
+
+fit_GP <- function(x, y, x_star, n_draws = 10L){
+  k <- gpflow$kernels$RBF(1L, lengthscales = 1.0)
+  k$variance$prior <- gpflow$priors$Gamma(1.0, 1.0)
+  k$lengthscales$prior <- gpflow$priors$Gamma(1.0, 1.0)
+  m <- gpflow$gpr$GPR(cbind(x), cbind(y), k)
+  m$likelihood$variance <- 0.05
+  m$optimize()
+  # print(k$lengthscales)
+  pred_mat <- m$predict_f_samples(cbind(x_star), n_draws)
+  pred_mat %>%
+    reshape2::melt() %>%
+    rename(index = Var2, draw = Var1, y = value) %>%
+    mutate(x = x_star[index])
+}
+
+fit_and_plot_GP <- function(x0, y0, x_star){
+  df_pred <- fit_GP(x0, y0, x_star, n_draws = 50L)
+  df_obs <- data.frame(x = x0, y = y0)
+  df_pred %>%
+    ggplot(aes(x, y)) +
+    geom_line(aes(group = draw), alpha = 0.25) +
+    geom_point(data = df_obs, col = "#b2182b", size = 3) +
+    theme_classic()
+}
